1a1632e5e8e012fec59b6aae6397868d840a2db4
David <dmcmanam@192.168.0.5>
2017-10-21 10:28:34 -0400

    WAVL insert tests.

---
+++ src/main/test/bbst_showdown/TreeMapWAVLTest.java
@@ -0,0 +1,108 @@
+package bbst_showdown;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TreeMapWAVLTest {
+
+    @Test
+    public void testOneLeftRotation() {
+	TreeMapWAVL<Integer, Integer> x = new TreeMapWAVL<>();
+	x.put(1, 1);
+	x.put(2, 2);
+	x.put(3, 3);
+
+	assertEquals(1, x.root.rank);
+	assertEquals(2, (int) x.root.value);
+	assertEquals(0, x.root.right.rank);
+    }
+
+    @Test
+    public void testTwoLeftRotations() {
+	TreeMapWAVL<Integer, Integer> x = new TreeMapWAVL<>();
+	x.put(1, 1);
+	x.put(2, 2);
+	x.put(3, 3);
+	x.put(4, 4);
+	x.put(5, 5);
+
+	assertEquals(2, x.root.rank);
+	assertEquals(2, (int) x.root.value);
+	assertEquals(2, x.rotations);
+	assertEquals(1, x.root.right.rank);
+	assertEquals(0, x.root.left.rank);
+    }
+
+    @Test
+    public void testThreeLeftRotations() {
+	TreeMapWAVL<Integer, Integer> x = new TreeMapWAVL<>();
+	x.put(1, 1);
+	x.put(2, 2);
+	x.put(3, 3);
+	x.put(4, 4);
+	x.put(5, 5);
+	x.put(6, 6);
+
+	assertEquals(3, x.rotations);
+	assertEquals(4, (int) x.root.value);
+	assertEquals(2, x.root.rank);
+	assertTrue(x.root.right.rank == 1 && x.root.left.rank == 1);
+    }
+
+    @Test
+    public void testLeftRightRotation() {
+	TreeMapWAVL<Integer, Integer> x = new TreeMapWAVL<>();
+	x.put(3, 3);
+	x.put(1, 1);
+	x.put(2, 2);
+
+	assertEquals(2, x.rotations);
+	assertEquals(2, (int) x.root.value);
+	assertEquals(1, x.root.rank);
+    }
+
+    @Test
+    public void testRightLeftRotation() {
+	TreeMapWAVL<Integer, Integer> x = new TreeMapWAVL<>();
+	x.put(3, 3);
+	x.put(6, 6);
+	x.put(4, 4);
+
+	assertEquals(2, x.rotations);
+	assertEquals(4, (int) x.root.value);
+	assertEquals(1, x.root.rank);
+    }
+
+    @Test
+    public void testBuildFibonacciTree() {
+	TreeMapWAVL<Integer, Integer> x = new TreeMapWAVL<>();
+	x.put(8, 8);
+	x.put(5, 5);
+	x.put(11, 11);
+	// 3,7,10,12
+	x.put(3, 3); x.put(7, 7); x.put(10, 10); x.put(12, 12);
+	// 2,4,6,9
+	x.put(2, 2); x.put(4, 4); x.put(6, 6); x.put(9, 9);
+	x.put(1, 1);
+	System.out.println("Rotations: " + x.rotations);
+
+	assertEquals(0, x.rotations);
+    }
+
+    @Test
+    public void testTwoRightRotations() {
+	TreeMapWAVL<Integer, Integer> x = new TreeMapWAVL<>();
+	x.put(5, 5);
+	x.put(4, 4);
+	x.put(3, 3);
+	x.put(2, 4);
+	x.put(1, 1);
+
+	assertEquals(2, x.rotations);
+	assertEquals(2, x.root.rank);
+	assertEquals(4, (int) x.root.value);
+	assertEquals(0, x.root.right.rank);
+	assertEquals(1, x.root.left.rank);
+    }
+}
